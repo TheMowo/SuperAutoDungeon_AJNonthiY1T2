2809503d23ce31ea8738fd9c8e33291e25d71302
NeedHelp <rachatafewfew@gmail.com>
2025-03-05 12:19:41 +0700

    Added Inventory Slots & Placeholder Items

---
+++ Assets/Scripts/FewScripts/InventoryItem.cs
@@ -0,0 +1,29 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public UnityEngine.UI.Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Begin Drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Dragging");
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("End Drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+    }
+}
---
+++ Assets/Scripts/FewScripts/InventorySlot.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        //This checks if the item slot is empty
+        if(transform.childCount == 0)
+        {
+            // If the item slot is empty, drop the item in the slot
+
+            // Gets the item being dragged
+            GameObject droppedItem = eventData.pointerDrag;
+            InventoryItem draggableItem = droppedItem.GetComponent<InventoryItem>();
+
+            // Drops the item in the slot
+            draggableItem.parentAfterDrag = transform;
+        }
+        else
+        {
+            // If the item slot is occupied by another item, swap them
+            
+            // Gets the item being dragged
+            GameObject droppedItem = eventData.pointerDrag;
+            InventoryItem draggableItem = droppedItem.GetComponent<InventoryItem>();
+
+            // Gets the current item in the slot
+            GameObject current = transform.GetChild(0).gameObject;
+            InventoryItem currentDraggable = current.GetComponent<InventoryItem>();
+
+            // Swaps the items
+            currentDraggable.transform.SetParent(draggableItem.parentAfterDrag);
+            draggableItem.parentAfterDrag = transform;
+        }
+    }
+}
