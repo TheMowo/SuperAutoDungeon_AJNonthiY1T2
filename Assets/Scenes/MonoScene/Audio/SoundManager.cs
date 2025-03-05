1a93cad40a0cb3304a6540a2c636e50c27c7dbb9
TheMowo <157827650+TheMowo@users.noreply.github.com>
2025-03-05 14:15:21 +0700

    Added MonoScene folder

---
+++ Assets/Scenes/MonoScene/Audio/SoundManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //Converts to Singleton
+    public static SoundManager _instance; private void Awake() { if (_instance != null) Destroy(this.gameObject); DontDestroyOnLoad(this.gameObject); _instance = this; }
+
+    [SerializeField] private AudioSource _soundFxSource;
+
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform)
+    {
+        //spawn in GameObject
+        AudioSource _audioSource = Instantiate(_soundFxSource, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioClip
+        _audioSource.clip = audioClip;
+
+        //play sound
+        _audioSource.Play();
+
+        //get length of sound fx clip
+        float clipLength = _audioSource.clip.length;
+
+        //destroy the clip after it is done playing
+        Destroy(_audioSource.gameObject, clipLength);
+    }
+
+    public void PlaySoundFXClipWithPitchChange(AudioClip audioClip)
+    {
+
+    }
+}
