37b1f828ddbf6e9b51e59316faa41da9d5c172e6
simp <113957483+matt2341@users.noreply.github.com>
2025-03-12 05:15:19 +0700

    Update GameData.cs

--- Assets/Scripts/MattScript/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/MattScript/DataPersistence/Data/GameData.cs
@@ -9,10 +9,12 @@ public class GameData
     public int ScoreCount;
 
     public AttributesData playerAttributesData;
+    public AttributesDataEnemy enemyAttributesData;
 
     public GameData()
     {
         this.ScoreCount = 0;
         this.playerAttributesData = new AttributesData();
+        this.enemyAttributesData = new AttributesDataEnemy();
     }
 }
