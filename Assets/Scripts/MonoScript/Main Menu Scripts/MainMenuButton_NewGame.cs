adf610e13c9a240fb15e9bf1e716ec506055540c
TheMowo <157827650+TheMowo@users.noreply.github.com>
2025-05-14 00:30:46 +0700

    Made start button load first scene 🗿

--- Assets/Scripts/MonoScript/Main Menu Scripts/MainMenuButton_NewGame.cs
+++ Assets/Scripts/MonoScript/Main Menu Scripts/MainMenuButton_NewGame.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButton_NewGame : MonoBehaviour
 {
@@ -6,4 +7,17 @@ public class MainMenuButton_NewGame : MonoBehaviour
     // If it does, prompt to confirm before proceeding
     // Resets save file
     // Starts new game, transitioning into Scene 1
+    private bool hasSave;
+
+    public void NewGame_ButtonPressed()
+    {
+        if (hasSave)
+        {
+            //prompt confirmation
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
