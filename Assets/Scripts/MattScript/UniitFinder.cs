380ebd771cae3b329367e9d23c0450e7b3847899
matt2341 <aphiphu.m13@gmail.com>
2025-05-14 13:39:05 +0700

    Fix the bug plus for you time :D

--- Assets/Scripts/MattScript/UniitFinder.cs
+++ Assets/Scripts/MattScript/UniitFinder.cs
@@ -15,5 +15,6 @@ public class UniitFinder : MonoBehaviour
         playerSaveSystem.GetAllPlayerUnitList();
         itemSaveSystem = FindFirstObjectByType<ItemSaveSystem>();
         itemSaveSystem.GetAllInventorySlotList();
+        itemSaveSystem.ItemLoad();
     }
 }
--- Assets/Scripts/ShereenScripts/CombatSystem.cs
+++ Assets/Scripts/ShereenScripts/CombatSystem.cs
@@ -17,6 +17,7 @@ public class CombatSystem : MonoBehaviour
     public Transform battlePoint;
     public enemySpawner enemySpawner;
     public ShopManager shopManager;
+    public bool isWin;
 
     public PlayerSaveSystem PSS;
     public ItemSaveSystem TSS;
@@ -119,6 +120,7 @@ public class CombatSystem : MonoBehaviour
                 Debug.Log("Player Wins");
                 ResetPlayerHP();
                 RepositionUnits();
+                isWin = true;
 
                 shopManager.OpenShopOnWin();
                 SSS.GetAllShopSlotList();
@@ -163,6 +165,7 @@ public class CombatSystem : MonoBehaviour
             Debug.Log("No one died, Player Wins");
             ResetPlayerHP();
             RepositionUnits();
+            isWin = true;
 
             shopManager.OpenShopOnWin();
             SSS.GetAllShopSlotList();
