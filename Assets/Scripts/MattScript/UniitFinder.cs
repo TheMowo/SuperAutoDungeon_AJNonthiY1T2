0a8f7b17e3999315ec677b1e74af01e0c9ef5848
matt2341 <aphiphu.m13@gmail.com>
2025-05-14 16:20:13 +0700

    SaveCurrency

--- Assets/Scripts/MattScript/UniitFinder.cs
+++ Assets/Scripts/MattScript/UniitFinder.cs
@@ -5,6 +5,7 @@ public class UniitFinder : MonoBehaviour
     public EnemySaveSystem enemySaveSystem;
     public PlayerSaveSystem playerSaveSystem;
     public ItemSaveSystem itemSaveSystem;
+    public CurrencySaveSystem currencySaveSystem;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,10 +13,12 @@ public class UniitFinder : MonoBehaviour
         itemSaveSystem = FindFirstObjectByType<ItemSaveSystem>();
         playerSaveSystem = FindFirstObjectByType<PlayerSaveSystem>();
         enemySaveSystem = FindFirstObjectByType<EnemySaveSystem>();
+        currencySaveSystem = FindFirstObjectByType<CurrencySaveSystem>();
         itemSaveSystem.GetAllInventorySlotList();
         enemySaveSystem.GetAllPlayerUnitList();
         playerSaveSystem.GetAllPlayerUnitList();
         playerSaveSystem.PlayerLoad();
         itemSaveSystem.ItemLoad();
+        currencySaveSystem.CurrencyLoad();
     }
 }
