e760dd65b0aefe03c5b9033e967c6ccc24edd0ee
TheMowo <157827650+TheMowo@users.noreply.github.com>
2025-05-21 19:24:57 +0700

    Item Pickup and Drop SFX + Item Sound script

---
+++ Assets/ItemSound.cs
@@ -0,0 +1,27 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSound : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    //Highly unoptimized script tbh
+    AudioSource PickUpSfx;
+    AudioSource DropSfx;
+    SoundManager soundManager;
+
+    void Start()
+    {
+        soundManager = FindFirstObjectByType<SoundManager>().GetComponent<SoundManager>();
+        PickUpSfx = GameObject.Find("SfxSource_ItemPickUp").GetComponent<AudioSource>();
+        DropSfx = GameObject.Find("SfxSource_ItemDrop").GetComponent<AudioSource>();
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        soundManager.PlaySfxClipWithPitchChange(PickUpSfx.clip);
+    }
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        soundManager.PlaySfxClipWithPitchChange(DropSfx.clip);
+    }
+}
