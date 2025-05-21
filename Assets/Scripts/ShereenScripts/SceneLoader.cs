ecea6accb97df1178c202365b64331f2cd257add
matt2341 <aphiphu.m13@gmail.com>
2025-05-21 20:30:43 +0700

    WAHHHHHH

--- Assets/Scripts/ShereenScripts/SceneLoader.cs
+++ Assets/Scripts/ShereenScripts/SceneLoader.cs
@@ -6,18 +6,21 @@ public class SceneLoader : MonoBehaviour
     public ItemSaveSystem ISS;
     public CurrencySaveSystem CSS;
     public PlayerSaveSystem Player;
+    public GameSettingSaveSystem GSS;
 
     public void LoadNextScene()
     {
         ISS = FindFirstObjectByType<ItemSaveSystem>();
         Player = FindFirstObjectByType<PlayerSaveSystem>();
         CSS = FindFirstObjectByType<CurrencySaveSystem>();
-        
-        if(ISS != null)
+        GSS.combatSystem = FindFirstObjectByType<CombatSystem>();
+
+        if (ISS != null)
         {
             ISS.ItemSaveData();
             CSS.CurrencySaveData();
             Player.PlayerSaveData();
+            GSS.GetSaveData();
         }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
