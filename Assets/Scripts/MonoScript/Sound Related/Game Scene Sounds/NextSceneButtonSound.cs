bd2588ae89e87081fe982574d5774d9c160732ce
matt2341 <aphiphu.m13@gmail.com>
2025-05-21 22:58:36 +0700

    Revert "Merge branch 'MonosBranch'"
    This reverts commit 9740dca949349f9e299a1d74112f91579275b58e, reversing
    changes made to 5a82e1586bdfe7f3c8d3bf54e1f6f31b0f55f31b.

--- Assets/Scripts/MonoScript/Sound Related/Game Scene Sounds/NextSceneButtonSound.cs
+++ Assets/Scripts/MonoScript/Sound Related/Game Scene Sounds/NextSceneButtonSound.cs
@@ -5,14 +5,16 @@ public class NextSceneButtonSound : MonoBehaviour, IPointerDownHandler
 {
     //I recommend you avert your eyes from this wacky solution to the sound problem-
     AudioClip Sfx;
+    SoundManager soundManager;
 
     void Start()
     {
+        soundManager = FindAnyObjectByType<SoundManager>();
         Sfx = GameObject.Find("UiSource_OnNext").GetComponent<AudioSource>().clip;
     }
 
     public void OnPointerDown(PointerEventData pointerEventData)
     {
-        SoundManager.Instance.PlaySfxClipWithPitchChange(Sfx);
+        soundManager.PlaySfxClipWithPitchChange(Sfx);
     }
 }
