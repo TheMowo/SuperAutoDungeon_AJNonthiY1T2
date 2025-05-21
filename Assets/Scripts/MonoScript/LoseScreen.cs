9c4e6a556054503c2bebcae38afc9a6b92249549
TheMowo <157827650+TheMowo@users.noreply.github.com>
2025-05-21 23:36:11 +0700

    Fixed a small bug + added lose screen & functionality

--- Assets/Scripts/MonoScript/LoseScreen.cs
+++ Assets/Scripts/MonoScript/LoseScreen.cs
@@ -3,6 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class LoseScreen : MonoBehaviour
 {
+    CheckPlayerLose _checkPlayerLose;
+    [SerializeField] GameObject loseScreen;
+    private void Start()
+    {
+        _checkPlayerLose = FindFirstObjectByType<CheckPlayerLose>();
+    }
+    private void Update()
+    {
+        //if (_checkPlayerLose.isOver)
+        {
+            loseScreen.SetActive(true);
+        }
+    }
     public void BackToMenu()
     {
         SceneManager.LoadScene(0);
